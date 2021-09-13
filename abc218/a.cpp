d6147361b47fe35a38f74935d1f62bab1d2ea41c
SayakaNakajima <andsayaka@gmail.com>
2021-09-13 12:16:18 +0900

    Fix 218-a

--- abc218/a.cpp
+++ abc218/a.cpp
@@ -4,12 +4,12 @@ using namespace std;
 #define REP(i, n) for (int i = 0; i < (n); ++i)
 
 /* 未参加 */
+// 最初、cout << s[n-1] << endl;だけで提出して間違う羽目に
+// 気を付けよう・・・・
 
 int main() {
   int n;
   string s;
   cin >> n >> s;
   cout << (s[n-1] == 'o' ? "Yes" : "No") << endl;
-  // 最初、cout << s[n-1] << endl;だけで提出して間違う羽目に
-  // 気を付けよう・・・・
 }
