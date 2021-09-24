fe33bec290bb2ef907ac6b4f6655eb1c4f96ae85
SayakaNakajima <andsayaka@gmail.com>
2021-09-24 11:50:32 +0900

    Change REP macro

--- template/abc.cpp
+++ template/abc.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define REP(i, a, n) for (int i = (a); i < (n); ++i)
 #define REP(i, n) for (int i = 0; i < (n); ++i)
 using ll = long long;
 int main() {
