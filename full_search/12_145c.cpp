09ee2f6d302132a6fbcccb502f8f4007ddad48b0
SayakaNakajima <andsayaka@gmail.com>
2021-09-28 22:34:05 +0900

    Add mutation search

---
+++ full_search/12_145c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, a, n) for (int i = (a); i < (n); ++i)
+#define ALL(x) (x).begin(),(x).end()
+
+/* 順列の全探索 */
+/* 良かった、ビット全探索よりわかりやすかった */
+
+int N;
+vector<int> np;
+vector<pair<int, int>> coordinate;
+
+int main() {
+  cin >> N;
+  REP (i, 0, N) {
+    int x, y;
+    cin >> x >> y;
+    coordinate.push_back(make_pair(x, y));
+    np.push_back(i);
+  }
+
+  double avg = 0;
+  int cnt = 0;
+  do {
+    REP (i, 0, N-1) {
+      int X = abs(coordinate.at(np[i]).first - coordinate.at(np[i+1]).first),
+      Y = abs(coordinate.at(np[i]).second - coordinate.at(np[i+1]).second);
+      avg += sqrt(pow(X, 2) + pow(Y, 2));
+    }
+    ++cnt;
+  } while (next_permutation(ALL(np)));
+  cout << fixed << setprecision(8);
+  cout << avg / cnt << endl;
+}
