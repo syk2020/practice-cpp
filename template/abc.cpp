aabcea9c771bfde4e779989c3381aa846101496a
SayakaNakajima <andsayaka@gmail.com>
2021-09-29 18:04:12 +0900

    Change template

--- template/abc.cpp
+++ template/abc.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define REP(i, a, n) for (int i = (a); i < (n); ++i)
-#define RREP(i, a, n) for (int i = (a); i >= (b); --i)
 #define FORE(i, a) for (auto &i : a)
 #define ALL(x) (x).begin(),(x).end()
-using ll = long long; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+#define RREP(i, a, n) for (int i = (a); i >= (b); --i)
+using ll = long long;
+const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
 
